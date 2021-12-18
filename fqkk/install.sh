032250aed7bc92b6351eb8844bdbf52bd4677c68
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2021-12-18 17:21:24 +0800

    Update install.sh

--- fqkk/install.sh
+++ fqkk/install.sh
@@ -3,20 +3,21 @@ if [ ! -d "fqkk" ]; then
 mkdir fqkk
 fi
 cd ~/fqkk
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqfr.sh)" > fqfr.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqfrdr.sh)" > fqfrdr.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqfrdr2.sh)" > fqfrdr2.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqrft.sh)" > fqrft.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqrk.sh)" > fqrk.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqrw.sh)" > fqrw.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqtx.sh)" > fqtx.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqzlm.sh)" > fqzlm.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/joj.sh)" > joj.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/owa.sh)" > owa.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/owar.sh)" > owar.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/owsq.sh)" > owsq.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/roj.sh)" > roj.sh
-echo "$(curl -s https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/run.sh)" > run.sh
+rm -rf fqfr.sh fqfrdr.sh fqfrdr2.sh fqrft.sh fqrk.sh fqrw.sh fqtx.sh fqzlm.sh joj.sh owa.sh owar.sh owsq.sh roj.sh run.sh
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqfr.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqfrdr.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqfrdr2.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqrft.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqrk.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqrw.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqtx.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/fqzlm.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/joj.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/owa.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/owar.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/owsq.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/roj.sh"
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/shjb/main/fqkk/run.sh"
 chmod +x fqfr.sh fqfrdr.sh fqfrdr2.sh fqrft.sh fqrk.sh fqrw.sh fqtx.sh fqzlm.sh joj.sh owa.sh owar.sh owsq.sh roj.sh run.sh
 cd ~
 echo "cd ~/fqkk && ./run.sh" > fqkk.sh
