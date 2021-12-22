e2a6f49dcca5f2c994315b58d732d3ea7e559095
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2021-12-22 09:31:13 +0800

    Update owar.sh

--- fqkk/owar.sh
+++ fqkk/owar.sh
@@ -1,18 +1,15 @@
-if [ ! -s ckerror.txt -a -s jkey.txt ]
-then ja=$(cat ja.txt)
+if [ ! -s ckerror.txt -a -s uuid.txt ]
+then uuid=$(cat uuid.txt)
+ja=$(cat ja.txt)
 ju=$(cat ju.txt)
 ji=$(cat ji.txt)
 js=$(cat js.txt)
-aou=$(cat aou.txt)
+ou=$(cat ou.txt)
+arou=$(cat arou.txt)
 key=$(cat key.txt)
 ov=$(cat ov.txt)
 op=$(cat op.txt)
 ua=$(cat ua.txt)
-uuid=$(curl -H "Host: open.weixin.qq.com" -H "upgrade-insecure-requests: 1" -H "user-agent: $ua" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "sec-fetch-site: none" -H "sec-fetch-mode: navigate" -H "sec-fetch-user: ?1" -H "sec-fetch-dest: document" -H "accept-language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7" -s --compressed "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$ja&redirect_uri=http%3A%2F%2F$ju%2Ffast_reada%2Foiejr%3Fjumpid%3D$ji&response_type=code&scope=snsapi_userinfo&state=$js&connect_redirect=1&uin=$aou&key=$key&version=$ov&pass_ticket=$op" | grep -o "&uuid.*" | cut -d '&' -f2 | cut -d '=' -f2)
-echo $uuid > uuid.txt
-uuid=$(cat uuid.txt)
-if [ ! -n "$uuid" ]
-then dt=$(date '+%Y-%m-%d %H:%M:%S')
-echo $dt owsq常量失效，请重新运行脚本并输入新的owsq > ckerror.txt
-fi
+curl -k -i --raw -o owar.txt "https://open.weixin.qq.com/connect/oauth2/authorize_reply?allow=1&snsapi_userinfo=on&uuid=$uuid&uin=$ou&key=$key&pass_ticket=$op&version=$ov" -H "Host: open.weixin.qq.com" -H "Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: $ua" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Sec-Fetch-Site: same-origin" -H "Sec-Fetch-Mode: navigate" -H "Sec-Fetch-Dest: document" -H "Referer: https://open.weixin.qq.com/connect/oauth2/authorize?appid=$ja&redirect_uri=http%%3A%%2F%%2F$ju%%2Ffast_reada%%2Foiejr%%3Fjumpid%%3D$ji&response_type=code&scope=snsapi_userinfo&state=$js&connect_redirect=1&uin=$arou&key=$key&version=$ov&pass_ticket=$op" -H "Accept-Encoding: gunzip, br" -H "Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7" -s
+echo $(cat owar.txt | grep -o "code.*" | cut -d '&' -f1 | cut -d '=' -f2) > jc.txt
 fi
