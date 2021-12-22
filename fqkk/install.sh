24826f0349fb1dbd8746b6bbe475df4e9e8dbccb
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2021-12-22 13:25:35 +0800

    Update install.sh

--- fqkk/install.sh
+++ fqkk/install.sh
@@ -2,13 +2,17 @@ cd ~
 read -p "如你确认已安装wget，请输入脚本地址开始安装脚本: " url
 echo $url > url.txt
 if [ $(grep -c "http" url.txt) -eq 0 ]
-then rm -rf url.txt && echo 脚本地址未包含http://，请重新运行脚本
+then rm -rf url.txt
+echo 脚本地址未包含http://，请重新运行脚本
 elif [ $(grep -c "raw" url.txt) -eq 0 ]
-then rm -rf url.txt && echo 脚本地址不是包含raw的直链，请重新运行脚本
+then rm -rf url.txt
+echo 脚本地址不是包含raw的直链，请重新运行脚本
 elif [ $(grep -c "shoujiyanxishe" url.txt) -eq 0 ]
-then rm -rf url.txt && echo 你确定这是我耳兔的脚本吗？别玩我了
+then rm -rf url.txt
+echo 你确定这是我耳兔的脚本吗？别玩我了
 elif [ $(grep -c "install" url.txt) -eq 0 ]
-then rm -rf url.txt && echo 真粗心，这可不是安装脚本的地址哦
+then rm -rf url.txt
+echo 真粗心，这可不是安装脚本的地址哦
 fi
 if [ -s url.txt ]
 then wurl=$(echo ${url%install*})
@@ -38,7 +42,8 @@ chmod +x $jbm
 rm -rf curl.txt install.sh wjb.sh
 cd ~
 rm -rf url.txt
-echo "cd ~/$wjj && ./run.sh" > $wjj.sh
+echo "cd ~/$wjj
+./run.sh" > $wjj.sh
 chmod +x $wjj.sh
 clear
 echo 脚本已安装完毕，请输入 ./$wjj.sh 运行脚本
