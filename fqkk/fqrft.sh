09d4ea9d051cec84cd40510ec18f853d9a1eca9e
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2021-12-29 12:26:49 +0800

    Update fqrft.sh

--- fqkk/fqrft.sh
+++ fqkk/fqrft.sh
@@ -2,6 +2,9 @@ if [ ! -s ckerror.txt ]
 then ua=$(cat ua.txt)
 ck=$(cat ck.txt)
 rw=$(cat rw.txt)
+if [ -s un.txt ]
+then un=$(cat un.txt)
+fi
 curl -k -i --raw -o fqrft.txt -X POST -d "readLastKey=" "http://$rw/reada/finishTask" -H "Host: $rw" -H "Proxy-Connection: keep-alive" -H "Accept: */*" -H "Origin: http://$rw" -H "X-Requested-With: XMLHttpRequest" -H "User-Agent: $ua" -H "Content-Type: application/x-www-form-urlencoded" -H "Cookie: $ck" -H "Referer: http://$rw/fast_reada" -H "Accept-Encoding: gunzip, deflate" -H "Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7" -s
 n=$(cat fqrft.txt | grep -o "num.*" | cut -d ',' -f1 | cut -d '"' -f3)
 s=$(cat fqrft.txt | grep -o "score.*" | cut -d ',' -f1 | cut -d '"' -f3)
@@ -24,6 +27,6 @@ dt=$(date '+%Y-%m-%d %H:%M:%S')
 if [ -s zlm.txt ]
 then zlg=$(cat zlg.txt)
 fi
-echo $dt $zlg今日已阅读$n次，今日总奖励$s币，本小时剩余未阅读$(cat rest.txt)次，当前$(cat y.txt)可提现 $msg > rft.txt
+echo $dt $un$zlg今日已阅读$n次，今日总奖励$s币，本小时剩余未阅读$(cat rest.txt)次，当前$(cat y.txt)可提现 $msg > rft.txt
 echo $status > status.txt
 fi
