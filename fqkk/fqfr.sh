32378555780c900731b4da31b5c5432e965bdb39
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2021-12-29 12:19:23 +0800

    Update fqfr.sh

--- fqkk/fqfr.sh
+++ fqkk/fqfr.sh
@@ -14,6 +14,7 @@ then c=0$zz
 else c=$zz
 fi
 echo $a.$c > y.txt
+echo $(cat fqfr.txt | grep -o 'userinfo-name".*' | cut -d '<' -f1 | cut -d '>' -f2) > un.txt
 else dt=$(date '+%Y-%m-%d %H:%M:%S')
 echo $dt ck无效，请重新运行脚本并输入新的常量 > ckerror.txt
 fi
