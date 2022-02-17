804e5a795f9e4147cb66f7c0024ccc56cb185e62
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2022-02-17 15:41:47 +0800

    Update ptkey.sh

--- jd/ptkey.sh
+++ jd/ptkey.sh
@@ -25,9 +25,12 @@ curl -s -k -i --raw -o xz -X POST -H "Host:$host" -H "Content-Length:$l" -H "Aut
 }
 xg(){
 _id=$(cat ev | grep -o "$pt_pin.*" | cut -d '"' -f5)
-if [ $(cat ev | grep -o "$pt_pin.*" | cut -d '"' -f21) = "remarks" ]
+r=$(cat ev | grep -o "$pt_pin.*" | cut -d '"' -f21)
+if [ ! -z $r ]
+then if [ $r = "remarks" ]
 then remarks=$(cat ev | grep -o "$pt_pin.*" | cut -d '"' -f23)
 ab="\"remarks\":\"$remarks\","
+fi
 fi
 t=$(expr $(date +%s%N) / 1000000)
 d="{\"name\":\"JD_COOKIE\",\"value\":\"$jdc\",$ab\"_id\":\"$_id\"}"
@@ -43,18 +46,36 @@ cl(){
 echo "需要上传ptkey到青龙的请输入青龙常量，格式为
 ip:端口或青龙地址@client_id@client_secret
 不需要的直接回车"
+stty -echo
 read -p "请输入青龙常量: " qlcl
+stty echo
 host=$(echo $qlcl | cut -d '@' -f1)
 client_id=$(echo $qlcl | cut -d '@' -f2)
 client_secret=$(echo $qlcl | cut -d '@' -f3)
 [ ! -z $qlcl ] && tk
 if [ -z $qlcl ]
-then read -p "请输入手机号: " mobile
+then stty -echo
+read -p "请输入手机号: " mobile
+stty echo
+a=$(echo $mobile | grep -o "[0-9]\+")
+b=$(echo $mobile | cut -c 1)
+if [ ${#mobile} -ne 11 -o ${#a} -ne 11 -o $b -ne 1 ]
+then echo $mobile不是正确的手机号
+mobile=""
+fi
 appid=959
 qversion=1.0.0
 country_code=86
 elif [ ! -z $tk ]
-then read -p "请输入手机号: " mobile
+then stty -echo
+read -p "请输入手机号: " mobile
+stty echo
+a=$(echo $mobile | grep -o "[0-9]\+")
+b=$(echo $mobile | cut -c 1)
+if [ ${#mobile} -ne 11 -o ${#a} -ne 11 -o $b -ne 1 ]
+then echo $mobile不是正确的手机号
+mobile=""
+fi
 appid=959
 qversion=1.0.0
 country_code=86
@@ -83,7 +104,7 @@ d="country_code=$country_code&client_ver=1.0.0&gsign=$gsign&appid=$appid&mobile=
 l=${#d}
 curl -s -k -i --raw -o sc -X POST -H "Host:qapplogin.m.jd.com" -H "cookie:$ck" -H "user-agent:Mozilla/5.0 (Linux; Android 10; V1838T Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/98.0.4758.87 Mobile Safari/537.36 hap/1.9/vivo com.vivo.hybrid/1.9.6.302 com.jd.crplandroidhap/1.0.3 ({"packageName":"com.vivo.hybrid","type":"deeplink","extra":{}})" -H "accept-language:zh-CN,zh;q=0.9,en;q=0.8" -H "content-type:application/x-www-form-urlencoded; charset=utf-8" -H "content-length:$l" -H "accept-encoding:" -d "$d" "https://qapplogin.m.jd.com/cgi-bin/qapp/quick"
 err_msg=$(cat sc | grep -o "err_msg.*" | cut -d '"' -f3)
-[ -z $err_msg ] && echo 手机号为$mobile的验证码发送成功 || echo $err_msg
+[ -z $err_msg ] && echo 验证码发送成功 || echo $err_msg
 fi
 }
 pt(){
@@ -100,12 +121,12 @@ if [ -z $err_msg ]
 then pt_key=$(cat pt | grep -o "pt_key.*" | cut -d '"' -f3)
 pt_pin=$(cat pt | grep -o "pt_pin.*" | cut -d '"' -f3)
 jdc="pt_key=$pt_key;pt_pin=$pt_pin;"
-echo 你的JD_COOKIE为 $jdc
 if [ ! -z $tk ]
-then echo 接下来为你上传至青龙
+then echo jdc获取成功接下来为你上传至青龙
 ev
+else echo 你的JD_COOKIE为 $jdc
 fi
-else echo $err_msg
+else echo $err_msg请检查$mobile是否正确
 fi
 fi
 }
